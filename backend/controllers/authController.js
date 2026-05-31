888573d6f29d38b7967fcaaf5c1a4f7a00b02445
unknown <mdakhtarlog@gmail.com>
2026-05-31 14:34:45 +0530

    feat: add register, verifyOTP and login in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -102,3 +102,56 @@ exports.verifyOTP = async (req,res)=>{
         })
     }
 }
+
+exports.login = async (req,res) =>{
+    try{
+        const {email, password} = req.body;
+
+        //find user by email
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message: "User not found Please register first",
+            })
+        }
+        //check if user is verified
+        if (!user.isVerified) {
+            return res.status(403).json({
+                success: false,
+                message: "Please verify your email before logging in."
+            })
+        }
+
+        //check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                success:false,
+                message: "Invalid credentials",
+            })
+        }
+        
+        //generate JWT token
+        const token = jwt.sign({userId:user._id, role: user.role, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        res.status(200).json({
+            success:true,
+            message: "Login successful",
+            token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                role: user.role,
+            }
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message: "Error occurred while logging in",
+            error: err.message
+        })
+    }
+}
